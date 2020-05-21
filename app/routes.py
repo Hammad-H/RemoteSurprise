0b3da589411d2bedbecf7a405a9c7ca0eed2d946
Hammad-H <hammad.hassan@richmond.edu>
2020-05-21 01:34:46 -0400

    Created the basic skeleton of the landing page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'Hammad'}
+    recommendations = [
+        {
+            'friend': {'username' : 'Mahad'},
+            'title': 'Two and a Half Men',
+            'type': 'Series'
+        },
+        {
+            'friend': {'username': 'Salar'},
+            'title': 'The Imitation Game',
+            'type': 'Movie'
+        }
+    ]
+
+    return render_template('index.html', title = 'Home', user = user, recommendations = recommendations)
